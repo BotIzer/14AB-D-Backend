d05a569447cc71fb46fcc307dbd2399383ad30f2
Lajtaib0801 <lajtaibenja@gmail.com>
2023-11-12 17:37:10 +0100

    Trims added and comments removed

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const createUser = async (req, res) => {
         const newUser = await User.create(req.body)
         res.status(201).json({ newUser })
     } catch (error) {
-        res.status(500).json({ msg: error }) //Error here
+        res.status(500).json({ msg: error })
     }
 }
 
