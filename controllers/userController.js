a43ce8e2e45d9b21ab8f4356072dbff8581a91fa
Lajtaib0801 <lajtaibenja@gmail.com>
2023-11-14 18:13:15 +0100

    userController's methods can be used now. Some refactor needed with trycatch and if (!user)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,10 @@
 const User = require('../models/userModel')
 
+const getUserIdFromUrl = (params) => {
+    const { userId: userId } = params
+    return userId
+}
+
 const createUser = async (req, res) => {
     try {
         const newUser = await User.create(req.body)
@@ -9,7 +14,68 @@ const createUser = async (req, res) => {
     }
 }
 
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(201).json({ users })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+const getUserDataById = async (req, res) => {
+    try {
+        const user = await User.findById(getUserIdFromUrl(req.params))
+        if (!user) {
+            return res.status(404).json({
+                msg: `No user found with id: ${getUserIdFromUrl(req.params)}`,
+            })
+        }
+        res.status(200).json({ user })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            getUserIdFromUrl(req.params),
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        )
+        if (!user) {
+            return res.status(404).json({
+                msg: `No user found with id: ${getUserIdFromUrl(req.params)}`,
+            })
+        }
+        res.status(200).json({ user })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(getUserIdFromUrl(req.params))
+        if (!user) {
+            return res.status(404).json({
+                msg: `No user found with id: ${getUserIdFromUrl(req.params)}`,
+            })
+        }
+        res.status(200).json({ user })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
 
 module.exports = {
     createUser,
+    getAllUsers,
+    getUserDataById,
+    updateUser,
+    deleteUser,
 }
