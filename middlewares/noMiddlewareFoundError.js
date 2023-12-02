17db7cd73c7cd4e617e962acc8461c6241b84af6
Lajtaib0801 <lajtaibenja@gmail.com>
2023-12-02 19:45:19 +0100

    noMiddlewareFoundError now responds with json

--- middlewares/noMiddlewareFoundError.js
+++ middlewares/noMiddlewareFoundError.js
@@ -1,6 +1,6 @@
 const notFound = (req, res) =>
     res
         .status(404)
-        .send(`Route does not exist: ${req.method} on '${req.originalUrl}' path`)
+        .json({message: `Route does not exist: ${req.method} on '${req.originalUrl}' path`})
 
 module.exports = notFound
