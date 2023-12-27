77118d43da44407bc5d78546e577afb1b548de3e
Lajtaib0801 <lajtaibenja@gmail.com>
2023-12-27 21:39:24 +0100

    noPermissionToUsePathError and protectPath added

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const { getAllUsers, getUserDataById, updateUser, deleteUser } = require('../controllers/userController')
 const loginUser = require('../controllers/loginController')
 const registerUser = require('../controllers/registerController')
+const protectPath = require('../middlewares/protectPath')
 
 
 router.route('/register').post(registerUser)
-router.route('/login').post(loginUser) //session-ökkel https://www.youtube.com/watch?v=zsOGmMuwhT4&t=200s
+router.route('/login').post(loginUser)
+router.route(protectPath, '/loggedInPage').get()
 
 module.exports = router
