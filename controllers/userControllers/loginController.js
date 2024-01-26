70486e51148f4de41a6c7afac42c66cba83afdad
Lajtaib0801 <lajtaibenja@gmail.com>
2024-01-26 10:51:27 +0100

    response check in loginController

--- controllers/userControllers/loginController.js
+++ controllers/userControllers/loginController.js
@@ -10,9 +10,9 @@ const loginUser = tryCatchWrapper(async (req, res) => {
         res.status(StatusCodes.UNAUTHORIZED).json({ message: `Username or password is wrong` })
         return
     }
-    sendTokenResponse(user, StatusCodes.OK, res)
+    res.status(StatusCodes.OK).json({}) //it stoppes here
+    // sendTokenResponse(user, StatusCodes.OK, res)
     return
 })
 
 module.exports = loginUser
-    
