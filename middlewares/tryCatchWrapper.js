ca66df83659e9222bbc082e67cd692be912d6842
Lajtaib0801 <lajtaibenja@gmail.com>
2024-01-30 22:17:08 +0100

    another version of tryCatch

--- middlewares/tryCatchWrapper.js
+++ middlewares/tryCatchWrapper.js
@@ -1,18 +1,31 @@
+// const tryCatchWrapper = (fn) => {
+//     return async (req, res, next) => {
+//         try {
+//             await fn(req, res, next)
+//         } catch (error) {
+//             console.log(error)
+//             next(error)
+//         }
+//     }
+//     // return async (...arguments) => {
+//     //     try {
+//     //         await fn(...arguments)
+//     //     } catch (error) {
+//     //         console.log(error)
+//     //         next(error)
+//     //     }
+//     // }
+// }
+
 const tryCatchWrapper = (fn) => {
-    // return async (req, res, next) => {
-    //     try {
-    //         await fn(req, res, next)
-    //     } catch (error) {
-    //         console.log(error)
-    //         next(error)
-    //     }
-    // }
-    return async (...arguments) => {
+    return async (...args) => {
         try {
-            await fn(...arguments)
+            await fn(...args)
         } catch (error) {
             console.log(error)
-            next(error)
+            if (typeof args[args.length - 1] === 'function') {
+                args[args.length - 1](error)
+            }
         }
     }
 }
