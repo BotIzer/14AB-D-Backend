694483b2373e4d6dc5e29e908106bc108875636c
Lajtaib0801 <lajtaibenja@gmail.com>
2024-02-09 18:51:44 +0100

    cannotConnectToDatabaseError added

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
+const cannotConnectToDatabaseError = require('../errors/databaseErrors/cannotConnectToDatabaseError')
 
 const connectDB = (url) => {
     try {
         mongoose.connect(url)
-        console.log("Database is connected!");
+        console.log('Database is connected!')
     } catch (error) {
-        console.log(error);
-        
+        console.log(error)
+        throw new cannotConnectToDatabaseError(error)
     }
-    
 }
 module.exports = connectDB
