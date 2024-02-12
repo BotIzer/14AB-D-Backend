4193fd998897991e327e4e7e3d412e1ba2fe08a1
markneu22 <markneu22@gmail.com>
2024-02-12 11:42:35 +0100

    protectedPath fixed

--- middlewares/protectPath.js
+++ middlewares/protectPath.js
@@ -5,11 +5,11 @@ const tryCatchWrapper = require('../middlewares/tryCatchWrapper')
 
 const protectPath = tryCatchWrapper(async (req, res, next) => {
     let token
-    if (!token) {
-        throw new noPermissionToUsePathError(req.originalUrl)
-    }
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1]
+    }
+    if (!token) {
+        throw new noPermissionToUsePathError(req.originalUrl)
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
