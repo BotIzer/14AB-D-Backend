9e5c37a9247a4ccce4b6175f98ff481ccdbcf469
Lajtaib0801 <lajtaibenja@gmail.com>
2024-02-12 18:59:51 +0100

    createChatController more features added

--- controllers/chatControllers/createChatController.js
+++ controllers/chatControllers/createChatController.js
@@ -6,16 +6,16 @@ const getCreatorIdFromHeaders = require('../../middlewares/getCreatorIdFromHeade
 const { daysToDieError } = require('../../errors/chatErrors/daysToDieError')
 
 const createChat = tryCatchWrapper(async (req, res) => {
-    const { name: name, is_ttl: isTtl, time_to_live_days: daysToDie, is_private: isPrivate } = req.body
+    const {
+        name: name,
+        is_ttl: isTtl,
+        time_to_live_days: daysToDie,
+        is_private: isPrivate,
+        other_user_name: otherUserName,
+    } = req.body
+    //If those 2 users already have the private chat, don't create another one should be implemented!
     const decodedCreatorId = await getCreatorIdFromHeaders(req.headers)
-    let expirationDate = null
-    if (isTtl) {
-        if (daysToDie < 1 && !Number.isInteger(daysToDie)) {
-            throw new daysToDieError()
-        }
-        expirationDate = new Date()
-        expirationDate.setDate(expirationDate.getDate() + daysToDie)
-    }
+    let expirationDate = setExpirationDate(isTtl, daysToDie)
     let newChat = new Chat({
         name: name,
         owner: decodedCreatorId,
@@ -29,8 +29,25 @@ const createChat = tryCatchWrapper(async (req, res) => {
     let creatorUser = await User.findById(decodedCreatorId)
     creatorUser.chats.push(newChat._id)
     creatorUser.save()
+    if (isPrivate) {
+        let otherUser = await User.findOne({ username: otherUserName })
+        otherUser.chats.push(newChat._id)
+        otherUser.save()
+    }
     res.status(StatusCodes.CREATED).json({ roomId: newChat._id })
     return
+})
+
+const setExpirationDate = tryCatchWrapper((ttl, daysToDie) => {
+    if (ttl) {
+        if (daysToDie < 1 && !Number.isInteger(daysToDie)) {
+            throw new daysToDieError()
+        }
+        let expirationDate = new Date()
+        expirationDate.setDate(expirationDate.getDate() + daysToDie)
+        return expirationDate
+    }
+    return null
 })
 
 module.exports = createChat
