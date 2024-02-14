a4e0910ed49fb4c9e757ef1d23ac2f522176e7fb
Lajtaib0801 <lajtaibenja@gmail.com>
2024-02-14 11:38:49 +0100

    1234

--- controllers/chatControllers/privateChatController.js
+++ controllers/chatControllers/privateChatController.js
@@ -1,17 +1,23 @@
 const tryCatchWrapper = require('../../middlewares/tryCatchWrapper')
 const Chat = require('../../models/chatroomModel')
+const User = require('../../models/userModel')
 const { chatShallBeCreatedError } = require('../../errors/chatErrors/chatErrors')
+const getCreatorIdFromHeaders = require('../../middlewares/getCreatorIdFromHeaders')
 
 const privateChat = tryCatchWrapper(async (req, res) => {
-    const { is_private: isPrivate, room_id: roomId } = req.body
-
-    if (roomId && isPrivate) {
-        const room = await Chat.findById(roomId)
-        if (room) {
-            res.status(200).json({ room })
-            return
+    let { friend: friendName, chat_id: chatId } = req.body
+    friendName = 'random'
+    if (chatId) { //van chat elvileg
+        //leellenőriz, hogy tényleg van-e
+        //van
+        const id = await getCreatorIdFromHeaders(req.headers)
+        const myFriends = (await User.findById(id).select('chats -_id')).chats
+        const friend = await User.findOne({ username: friendName }).select('_id')
+        if (friend) {
+            if (myFriends.includes(friend._id)) {
+                //Van közös chat
+            }
         }
-        throw new chatShallBeCreatedError()
     }
     return
 })
