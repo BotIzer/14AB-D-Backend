41cf4963ffea1a348d87f226abaf7aaac152d75e
Lajtaib0801 <lajtaibenja@gmail.com>
2024-02-15 11:17:37 +0100

    internal server error fixed on /chat when not logged in

--- middlewares/protectPath.js
+++ middlewares/protectPath.js
@@ -2,8 +2,13 @@ const jwt = require('jsonwebtoken')
 const { noPermissionToUsePathError } = require('../errors/userErrors/userErrors')
 const User = require('../models/userModel')
 const tryCatchWrapper = require('../middlewares/tryCatchWrapper')
+const otherUniqueError = require('../errors/otherUniqueError')
+const { StatusCodes } = require('http-status-codes')
 
 const protectPath = tryCatchWrapper(async (req, res, next) => {
+    if (req.headers.authorization == 'Bearer null') {
+        res.status(StatusCodes.UNAUTHORIZED).json({ message: 'You need to log in to access the chats page!' })
+    }
     let token
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1]
