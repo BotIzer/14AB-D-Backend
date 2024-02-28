ee88f5d2ec87636b3624b8c5593175c7e4ad13f6
Lajtaib0801 <lajtaibenja@gmail.com>
2024-02-28 17:57:20 +0100

    unnecessary comments removed from connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,17 +1,3 @@
-// const mongoose = require('mongoose')
-// const cannotConnectToDatabaseError = require('../errors/databaseErrors/cannotConnectToDatabaseError')
-
-// const connectDB = (url) => {
-//     try {
-//         mongoose.connect(url)
-//         console.log('Database is connected!')
-//     } catch (error) {
-//         console.log(error)
-//         throw new cannotConnectToDatabaseError(error)
-//     }
-// }
-// module.exports = connectDB
-
 const mongoose = require('mongoose')
 const cannotConnectToDatabaseError = require('../errors/databaseErrors/cannotConnectToDatabaseError')
 
