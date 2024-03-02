13f68dc5937230bc56a964fa9f538d1226a194fa
Lajtaib0801 <lajtaibenja@gmail.com>
2024-03-02 17:18:56 +0100

    protectPath added to /user DELETE

--- routes/router.js
+++ routes/router.js
@@ -44,7 +44,7 @@ const {
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/search').post(search)
-router.route('/user').put(protectPath, updateUser).delete(/*protectPath, */deleteUser) //DELETEUSER NOT TESTED
+router.route('/user').put(protectPath, updateUser).delete(protectPath, deleteUser) //DELETEUSER NOT TESTED
 router.route('/user/:username').get(getUserProfileByUsername)
 router.route('/user/addHobby').post(protectPath, addHobby)
 router.route('/user/friendRequests').get(protectPath, getUserRequests) //NOT TESTED!
