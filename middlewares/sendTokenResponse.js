3e811e09bb5c2bff0d7f5052d6d8b3e9dd8063c9
Lajtaib0801 <lajtaibenja@gmail.com>
2024-03-07 17:09:58 +0100

    /login added to swagger

--- middlewares/sendTokenResponse.js
+++ middlewares/sendTokenResponse.js
@@ -4,7 +4,7 @@ const sendTokenResponse = async (user, statusCode, res) => {
     const token = user.getSignedJwtToken()
 
     const userInfo = await getUserInfoFromToken(token)
-    res.json({ success: 'true', userInfo: userInfo, token: token})
+    res.json({ success: true, userInfo: userInfo, token: token})
     return
 }
 
