2887cde7b55ee5cd495e219be1755e55e5f2ba77
markneu22 <markneu22@gmail.com>
2024-03-11 10:27:00 +0100

    checkWetherBannedFromForum is finer than a Sunday

--- middlewares/checkWetherBannedFromForum.js
+++ middlewares/checkWetherBannedFromForum.js
@@ -5,7 +5,7 @@ const Forum = require('../models/forumModel')
 const checkWetherBannedFromForum = tryCatchWrapper(async(req, res, next) => {
     const forumId = req.params.forumId
     const userId = getCreatorIdFromHeaders(req.headers)
-    const forum = await Forum.findById(forumId)
+    const forum = await Forum.findOne({'_id.forum_id': forumId})
     if (forum.blacklist.includes(userId)) {
         res.status(403).json({ message: 'You are banned from this forum!' })
         return
