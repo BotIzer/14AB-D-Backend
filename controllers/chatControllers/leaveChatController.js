ae8b2e377be0be0584dccddffc59ca031b21128b
Lajtaib0801 <lajtaibenja@gmail.com>
2024-03-12 12:49:22 +0100

    leaveChatController updated, TEST NEEDED

--- controllers/chatControllers/leaveChatController.js
+++ controllers/chatControllers/leaveChatController.js
@@ -8,9 +8,12 @@ const leaveChat = tryCatchWrapper(async (req, res) => {
     const user = await User.findById(decodedId)
     user.chats.pull(chatId)
     user.save()
-    const chat = await Chat.findById(chatId)
-    chat.users.pull({ user_id: decodedId })
-    chat.save()
+    const chat = await Chat.updateOne({ _id: chatId }, { $pull: { users: { user_id: decodedId } } })
+    res.status(200).json({
+        success: true,
+        message: 'Chat left successfully!',
+        chat,
+    })
 })
 
 module.exports = leaveChat
