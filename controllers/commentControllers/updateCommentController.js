5bb9541915b3cf296296a8900a8605fcf0be0b71
Lajtaib0801 <lajtaibenja@gmail.com>
2024-03-19 21:06:05 +0100

    updateCommentController's status codes updated

--- controllers/commentControllers/updateCommentController.js
+++ controllers/commentControllers/updateCommentController.js
@@ -3,23 +3,22 @@ const Chatroom = require('../../models/chatroomModel')
 const tryCatchWrapper = require('../../middlewares/tryCatchWrapper')
 const { StatusCodes } = require('http-status-codes')
 const getCreatorIdFromHeaders = require('../../middlewares/getCreatorIdFromHeaders')
-const { noChatFoundError } = require('../../errors/chatErrors/chatErrors')
 
 const updateComment = tryCatchWrapper(async (req, res) => {
     const userId = await getCreatorIdFromHeaders(req.headers)
     const commentId = req.params.commentId
     const comment = await Comment.findById(commentId)
     if (!comment) {
-        res.status(404).json({ message: 'Comment not found' })
+        res.status(StatusCodes.NOT_FOUND).json({ message: 'Comment not found' })
     }
     if (comment._id.creator_id.toString() !== userId) {
-        res.status(401).json({ message: 'You cannot edit this comment!' })
+        res.status(StatusCodes.UNAUTHORIZED).json({ message: 'You cannot edit this comment!' })
     }
     if (req.body.text && req.body.text.length > 2000) {
-        res.status(400).json({ message: 'Comment too long' })
+        res.status(StatusCodes.BAD_REQUEST).json({ message: 'Comment too long' })
     }
     if (req.body._id || req.body.creation_date || req.body.likes || req.body.dislikes || req.body.emoticons) {
-        res.status(400).json({ message: 'Cannot edit this field!' })
+        res.status(StatusCodes.BAD_REQUEST).json({ message: 'Cannot edit this field!' })
     }
     comment.updateOne(req.body)
     await comment.save()
