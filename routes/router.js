40863b9155031d9f3e75521507cd7aa784deedf4
Lajtaib0801 <lajtaibenja@gmail.com>
2024-03-19 17:48:34 +0100

    'not tested's removed from router

--- routes/router.js
+++ routes/router.js
@@ -57,16 +57,16 @@ router.route('/user/friends/requests').get(protectPath, getUserRequests)
 router.route('/user/friends/sentRequests').get(protectPath, getUsersSentRequests)
 router.route('/user/:username').get(getUserProfileByUsername)
 router.route('/user/addHobby').post(protectPath, addHobby)
-router.route('/forum').get(getAllForums).post(protectPath, createForum).delete(protectPath, deleteForum) //DELETEFORUM NOT TESTED
+router.route('/forum').get(getAllForums).post(protectPath, createForum).delete(protectPath, deleteForum)
 router.route('/forum/getAllThreads/:forumId').get(checkWetherBannedFromForum, getAllThreads)
 router.route('/forum/getForumsByTag/:tag').get(searchForumByTag)
-router.route('/forum/:forumId').get(getForumById).put(protectPath, updateForum) //PUT NOT TESTED!
-router.route('/forum/ban').post(protectPath, banUserFromForum).put(protectPath, unbanUserFromForum) //NOT TESTED
+router.route('/forum/:forumId').get(getForumById).put(protectPath, updateForum)
+router.route('/forum/ban').post(protectPath, banUserFromForum).put(protectPath, unbanUserFromForum)
 router.route('/thread').post(protectPath, createThread)
-router.route('/thread/:threadId').delete(protectPath, deleteThreadConroller) //NOT TESTED
+router.route('/thread/:threadId').delete(protectPath, deleteThreadConroller)
 router.route('/chat/:chatId/comments').get(protectPath, getChatsComments)
 router.route('/chat/:chatId').get(protectPath, getChatDataById).delete(protectPath, deleteChat)
-router.route('/chat/addFriend').post(protectPath, addFriendToChat) //NOT TESTED!
+router.route('/chat/addFriend').post(protectPath, addFriendToChat)
 router.route('/createOrRetrieveChat').post(protectPath, checkMutualChat)
 router.route('/chat').post(protectPath, createChat)
 router.route('/chat/leaveChat').post(protectPath, leaveChat)
