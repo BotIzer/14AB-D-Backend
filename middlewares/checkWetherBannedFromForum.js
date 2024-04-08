3e03509e90994f1d9fe276a307bf0751e65f4b76
Lajtaib0801 <lajtaibenja@gmail.com>
2024-04-08 13:34:36 +0200

    checkWetherBannedFromForum updated

--- middlewares/checkWetherBannedFromForum.js
+++ middlewares/checkWetherBannedFromForum.js
@@ -4,6 +4,9 @@ const Forum = require('../models/forumModel')
 
 const checkWetherBannedFromForum = tryCatchWrapper(async(req, res, next) => {
     const forumId = req.params.forumId
+    if (req.headers.authorization == 'Bearer null') {
+        next()
+    }
     const userId = await getCreatorIdFromHeaders(req.headers)
     const forum = await Forum.findOne({'_id.forum_id': forumId})
     if (forum.blacklist.includes(userId)) {
