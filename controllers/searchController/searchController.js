2512cbc44743f0c98c058840682228c1ab9f93ba
Lajtaib0801 <lajtaibenja@gmail.com>
2024-04-10 09:07:47 +0200

    searchController now not returns the not verified user

--- controllers/searchController/searchController.js
+++ controllers/searchController/searchController.js
@@ -6,7 +6,7 @@ const tryCatchWrapper = require('../../middlewares/tryCatchWrapper')
 const search = tryCatchWrapper(async (req, res) => {
     const page = parseInt(req.query.page) || 0
     const limit = parseInt(req.query.limit) || 10
-    const skip = (page * 10)
+    const skip = page * 10
     const searchKeyword = req.body.keyword
     if (!searchKeyword || searchKeyword.trim() === '') {
         return res.status(StatusCodes.BAD_REQUEST).json({
@@ -25,6 +25,7 @@ const search = tryCatchWrapper(async (req, res) => {
     items.push(
         await User.find({
             username: { $regex: searchKeyword, $options: 'i' },
+            isVerified: true,
         })
             .skip(skip)
             .limit(limit)
