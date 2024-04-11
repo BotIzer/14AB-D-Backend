a3abd8953e1da62036f03e6f6ccb844df53a5328
Lajtaib0801 <lajtaibenja@gmail.com>
2024-04-11 09:06:19 +0200

    getAllThreadsByForumIdController pagination added

--- controllers/forumControllers/getAllThreadsByForumIdController.js
+++ controllers/forumControllers/getAllThreadsByForumIdController.js
@@ -3,14 +3,18 @@ const { StatusCodes } = require('http-status-codes')
 const Thread = require('../../models/threadModel')
 
 const getAllThreadsByForumId = tryCatchWrapper(async (req, res) => {
-    const threads = await Thread.find({ '_id.forum_id': req.params.forumId })
+    const page = parseInt(req.query.page) || 0
+    const limit = parseInt(req.query.limit) || 10
+    const skip = page * 10
+    const pagesCount = Math.ceil((await Thread.countDocuments()) / 10)
+    const threads = await Thread.find({ '_id.forum_id': req.params.forumId }).skip(skip).limit(limit)
     if (!threads) {
         res.status(StatusCodes.NOT_FOUND).json({
             message: 'No threads found',
         })
         return
     }
-    res.status(StatusCodes.OK).json(threads)
+    res.status(StatusCodes.OK).json({ pagesCount, threads })
     return
 })
 
