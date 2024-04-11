ef0fd232eb963841763a8c54edf3b42d1ce336d2
Lajtaib0801 <lajtaibenja@gmail.com>
2024-04-11 13:43:43 +0200

    recommend duplicated chats fixed

--- controllers/forumControllers/recommendForumsController.js
+++ controllers/forumControllers/recommendForumsController.js
@@ -5,20 +5,19 @@ const { StatusCodes } = require('http-status-codes')
 const recommendForums = tryCatchWrapper(async (req, res) => {
     let numberOfForums = req.query.numberOfForums || 5
     const numberOfDocuments = await Forum.countDocuments()
+
     if (numberOfDocuments < numberOfForums) {
         numberOfForums = numberOfDocuments
     }
-    const randomForums = []
-    for (let i = 0; i < numberOfForums; i++) {
-        let randomNumber = Math.floor(Math.random() * numberOfForums)
-        let forum = await Forum.findOne().skip(randomNumber).limit(1)
-        while (randomForums.some(f => f._id.forum_id == forum._id.forum_id)) {
-            randomNumber = Math.floor(Math.random() * numberOfForums)
-            forum = await Forum.findOne().skip(randomNumber).limit(1)
-        }
-        randomForums.push(forum)
+
+    const randomForums = new Set()
+    while (randomForums.size < numberOfForums) {
+        let randomNumber = Math.floor(Math.random() * numberOfDocuments)
+        const forum = await Forum.findOne().skip(randomNumber).limit(1)
+        randomForums.add(forum)
     }
-    return res.status(StatusCodes.OK).json(randomForums)
+
+    return res.status(StatusCodes.OK).json([...randomForums])
 })
 
 module.exports = recommendForums
