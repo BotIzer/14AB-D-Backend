5c8aad88220e3bf8a2ae28e1b3ace2d736f23d96
markneu22 <markneu22@gmail.com>
2024-04-12 12:47:24 +0200

    thread comments should work now

--- index.js
+++ index.js
@@ -77,8 +77,7 @@ const startServer = async () => {
                 }
             })
             commentChangeStream.on('change', async (change) => {
-                console.log()
-                if (await Thread.findOne({ '_id.thread_id': change._id.room_id })) {
+                if (await Thread.findOne({ '_id.thread_id': change._id.room_id }) !== undefined) {
                     return
                 }
                 io.emit('commentChange', change)
